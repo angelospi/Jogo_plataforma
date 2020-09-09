8e52bc3f5dd1823d27895063d192ba0007894be9
angelospi <angelo.spinardi@hotmail.com>
2020-09-09 20:44:47 -0300

    Adicionando movimento a camera

---
+++ Jogo_plataforma/Assets/scriptCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptCamera : MonoBehaviour
+{
+    public GameObject PC;
+    public float y_offset=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position= new Vector3(PC.transform.position.x,PC.transform.position.y+y_offset,-10);
+    }
+}
--- Jogo_plataforma/Assets/scriptMegaman.cs
+++ Jogo_plataforma/Assets/scriptMegaman.cs
@@ -7,19 +7,29 @@ public class scriptMegaman : MonoBehaviour
     private Rigidbody2D rbd;
     public float velocidade; 
     private Animator anim;
+    private bool dir=true;
+    private bool chao = true;
     // Start is called before the first frame update
     void Start()
     {
         rbd=GetComponent<Rigidbody2D>();
         velocidade=10;
         anim=GetComponent<Animator>();
+
     }
 
+    private void OnCollisionEnter2D(Collision2D collision){
+        chao=true;
+    }
+    private void OnCollisionExit2D(Collision2D collision){
+        chao=false;
+    }
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         rbd.velocity=new Vector2(x*velocidade,rbd.velocity.y);
+       
 
         if(x==0){
           anim.SetBool("move",false);
@@ -28,7 +38,12 @@ public class scriptMegaman : MonoBehaviour
             anim.SetBool("move",true);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(dir && x<0 || !dir && x>0 ){
+            transform.Rotate(new Vector2(0,180));
+            dir =!dir;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && chao){
             rbd.AddForce(new Vector2(0,250));
         }
 
